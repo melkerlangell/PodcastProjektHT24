b70b67d33d6b56785c15f25cb526de089ae752dc
Melker Langell <epicmannotch@gmail.com>
2024-10-14 17:10:54 +0200

    lol

--- PodcastProjektHT24/Form1.Designer.cs
+++ PodcastProjektHT24/Form1.Designer.cs
@@ -3,6 +3,7 @@
     partial class Form1
     {
         /// <summary>
+        /// test
         ///  Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
