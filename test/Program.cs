c13a7917e40ae4db9a1210173f704ea353e23cfd
noahallvall <146009156+noahallvall@users.noreply.github.com>
2024-10-16 11:07:58 +0200

    noaaa

--- test/Program.cs
+++ test/Program.cs
@@ -18,6 +18,8 @@ namespace test
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            //test push noa
         }
     }
 }
